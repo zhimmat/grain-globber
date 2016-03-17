147e93eaa1762451ba7aad9090a3a9ea655be74b
zhimmat <zeyad.himmat@live.com>
2016-03-16 20:53:42 -0400

    more code added to main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ grains.forEach(function (item) {
     var $p = $('<p>');
     var $img = $('<img>');
     var $h2 = $('<h2>');
+
+      $ul.append($li);
+      $img.attr('src', 'images/' + item.img);
+      $li.append($img);
+      $h2.html(item.name);
+      $li.append($h2);
+      $p.html(item.desc);
+      $li.append($p);
+      });
